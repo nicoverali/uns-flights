a9a30a1b1dcf57f324b864915f15c39ef4dcf6f5
nicoverali <nicolasalevera98@gmail.com>
2019-10-03 03:59:02 -0300

    Setup JavaFX with Gradle

---
+++ src/main/java/HelloFX.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class HelloFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX WebView Example");
+
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+
+        URL url = this.getClass().getClassLoader().getResource("index.html");
+        webEngine.load(url.toString());
+
+        VBox vBox = new VBox(webView);
+        Scene scene = new Scene(vBox, 960, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
