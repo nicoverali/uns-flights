475622856fb95e8e8f52f97c31c98b1e2a2a6a9e
nicoverali <nicolasalevera98@gmail.com>
2019-10-07 19:06:11 -0300

    Improve LocationFieldset validation
    It now uses IconTextInput states to help
    the user acknowledge the state of their input.
    
    It also autocompletes the input if the user left it uncomplete

--- web/src/components/FlightSearchForm/LocationFieldset/index.js
+++ web/src/components/FlightSearchForm/LocationFieldset/index.js
@@ -5,6 +5,8 @@ import Autosuggest from 'react-autosuggest';
 import IconTextInput from '@Components/FlightSearchForm/IconTextInput';
 import SuggestionsContainer from './SuggestionsContainer';
 import AirplaneIcon from '@Assets/icons/airplane.svg';
+import AirplaneTakeOffIcon from '@Assets/icons/airplane-takeoff.svg';
+import AirplaneLandingIcon from '@Assets/icons/airplane-landing.svg';
 
 
 // Imagine you have a list of languages that you'd like to autosuggest.
@@ -44,9 +46,11 @@ const languages = [
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
   
+    // TODO This should return an array with all matching suggestions
     return inputLength === 0 ? [] : languages.filter(lang =>
       lang.name.toLowerCase().slice(0, inputLength) === inputValue
     );
+    return []
   };
   
   // When suggestion is clicked, Autosuggest needs to populate the input
@@ -71,64 +75,133 @@ export default class LocationFieldset extends React.Component{
         // Suggestions also need to be provided to the Autosuggest,
         // and they are initially empty because the Autosuggest is closed.
         this.state = {
-          value: '',
-          suggestions: []
+          fromValue: '',
+          fromSuggestions: [],
+          toValue: '',
+          toSuggestions: []
         };
     }
 
-    onChange = (event, { newValue }) => {
+    onFromChange = (event, { newValue }) => {
         this.setState({
-            value: newValue
+            fromValue: newValue
         });
     };
+
+    onToChange = (event, { newValue }) => {
+      this.setState({
+          toValue: newValue
+      });
+  };
 
     // Autosuggest will call this function every time you need to update suggestions.
     // You already implemented this logic above, so just use it.
-    onSuggestionsFetchRequested = ({ value }) => {
+    onFromSuggestionsFetchRequested = ({ value }) => {
+        // TODO This gets called every time the user updates the value, you should return an array with all matching suggestions
+        // This behavior is already made in an upper method called 'getSuggestions(value)'
         this.setState({
-        suggestions: getSuggestions(value)
+          fromSuggestions: getSuggestions(value)
         });
     };
+
+    onToSuggestionsFetchRequested = ({ value }) => {
+      // TODO This gets called every time the user updates the value, you should return an array with all matching suggestions
+      // This behavior is already made in an upper method called 'getSuggestions(value)'
+      this.setState({
+      toSuggestions: getSuggestions(value)
+      });
+  };
 
     // Autosuggest will call this function every time you need to clear suggestions.
-    onSuggestionsClearRequested = () => {
+    onFromSuggestionsClearRequested = () => {
         this.setState({
-          suggestions: []
+          fromSuggestions: []
         });
     };
 
-    render(){
-        const { value, suggestions } = this.state;
+    onToSuggestionsClearRequested = () => {
+      this.setState({
+        toSuggestions: []
+      });
+    };
 
+    handleFromInputBlur = (event) =>{
+      if(event.target.value != ''){
+        this.setState({fromValue: this.findMatchFor(event.target.value)});
+      }
+    }
+
+    handleToInputBlur = (event) => {
+      if(event.target.value != ''){
+        this.setState({toValue: this.findMatchFor(event.target.value)});
+      }
+    }
+
+    isValidInput(inputValue){
+      if(languages.find((lan) => lan.name == inputValue) != undefined){
+        return true;
+      }
+      return false;
+    }
+
+    findMatchFor(value){
+      if(!this.isValidInput(value)){
+        let possibleMatch = languages.find(lan => lan.name.toLowerCase().startsWith(value.toLowerCase()));
+        if(possibleMatch != undefined){
+          return possibleMatch.name;
+        }
+        return value
+      }
+      return value;
+    }
+
+
+    
+
+    render(){
+        let fromInputValid = this.isValidInput(this.state.fromValue);
+        let toInputValid = this.isValidInput(this.state.toValue);
+
+        const fromProps = {
+          value: this.state.fromValue,
+          onChange: this.onFromChange,
+          onBlur: this.handleFromInputBlur,
+          icon: AirplaneTakeOffIcon,
+          label: "Origen",
+          error: !fromInputValid && this.state.fromValue != '',
+          valid: fromInputValid
+        }
         // Autosuggest will pass through all these props to the input.
-        const inputProps = {
-            placeholder: 'Type a programming language',
-            value,
-            onChange: this.onChange,
-            icon: AirplaneIcon,
-            label: "Origen"
+        const toProps = {
+          value: this.state.toValue,
+            onChange: this.onToChange,
+            onBlur: this.handleToInputBlur,
+            icon: AirplaneLandingIcon,
+            label: "Destino",
+            error: !toInputValid && this.state.toValue != '',
+            valid: toInputValid
         };
 
         return (
             <fieldset {...this.props} className={`location-fieldset-component ${this.props.className || ''}`}>
                 <Autosuggest
-                    suggestions={suggestions}
-                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    suggestions={this.state.fromSuggestions}
+                    onSuggestionsFetchRequested={this.onFromSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onFromSuggestionsClearRequested}
                     getSuggestionValue={getSuggestionValue}
                     renderSuggestion={renderSuggestion}
-                    inputProps={inputProps}
+                    inputProps={fromProps}
                     renderInputComponent={IconTextInput}
                     renderSuggestionsContainer={SuggestionsContainer}
                 />
 
                 <Autosuggest
-                    suggestions={suggestions}
-                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    suggestions={this.state.toSuggestions}
+                    onSuggestionsFetchRequested={this.onToSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onToSuggestionsClearRequested}
                     getSuggestionValue={getSuggestionValue}
                     renderSuggestion={renderSuggestion}
-                    inputProps={inputProps}
+                    inputProps={toProps}
                     renderInputComponent={IconTextInput}
                     renderSuggestionsContainer={SuggestionsContainer}
                 />
