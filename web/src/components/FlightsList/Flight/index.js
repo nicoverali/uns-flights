66480cada25ade6f0ee0265b2c8d77e2a4eaf49a
nicoverali <nicolasalevera98@gmail.com>
2019-10-08 02:59:09 -0300

    Add FlightComponent #1

---
+++ web/src/components/FlightsList/Flight/index.js
@@ -0,0 +1,60 @@
+import './index.scss';
+import React from 'react';
+import FlightEndPoint from './FlightEndPoint';
+import FlightInfoItem from './FlightInfoItem';
+import AirplaneIcon from '@Assets/icons/airplane.svg';
+import PrimaryButton from '@Components/PrimaryButton';
+
+export default class Flight extends React.Component{
+
+    render(){
+
+        return (
+            <div className={`flight-component ${this.props.className||''}`}>
+                
+                <div className="flight-main-content">
+                    <div className="flight-time">
+                        <FlightEndPoint className="flight-end-point"
+                            time="08:00"
+                            airportCode="BCN"
+                            airportName="Wiloxx Fox"
+                        />
+                        <div className="flight-route">
+                            <hr className="flight-route-line"></hr>
+                            <AirplaneIcon/>
+                        </div>
+                        <FlightEndPoint className="flight-end-point"
+                            time="14:30"
+                            airportCode="MIA"
+                            airportName="Wiloxx Fox"
+                        />
+                    </div>
+
+                    <div className="flight-info-and-button">
+                        <div className="flight-information">
+                            <FlightInfoItem 
+                                label="Nro vuelo"
+                                value="AC9217"
+                            />
+                            <FlightInfoItem 
+                                label="Nro vuelo"
+                                value="AC9217"
+                            />
+                            <FlightInfoItem 
+                                label="Nro vuelo"
+                                value="AC9217"
+                            />
+                        </div>
+
+                        <PrimaryButton className="flight-button" outline={true}>
+                            Ver clases
+                        </PrimaryButton>
+                    </div>
+                </div>
+                
+            </div>
+        );
+
+    }
+
+}
--- web/src/pages/AvailableFlights/index.js
+++ web/src/pages/AvailableFlights/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import React from 'react';
 import FlightsSearchForm from '@Components/FlightSearchForm';
+import Flight from '@Components/FlightsList/Flight';
 
 export default class AvailableFlights extends React.Component{
 
@@ -10,6 +11,8 @@ export default class AvailableFlights extends React.Component{
             <div id="available-flights-page">
                 <h2 className="available-flights-main-title">Consulta los vuelos disponibles</h2>
                 <FlightsSearchForm/>
+                <h2 className="available-flights-main-title">Vuelos de ida</h2>
+                <Flight/>
             </div>
         );
     }
