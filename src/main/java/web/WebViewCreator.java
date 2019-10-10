c3c1bea59d1658cf8ad72ba1e88e4a905f5e600a
nicoverali <nicolasalevera98@gmail.com>
2019-10-10 01:27:29 -0300

    Reorganize JavaFX WebView class and build.gradle

--- src/main/java/web/WebViewCreator.java
+++ src/main/java/web/WebViewCreator.java
@@ -2,7 +2,6 @@ package web;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
@@ -17,41 +16,35 @@ import netscape.javascript.JSObject;
 
 import java.net.URL;
 
-public class HelloFX extends Application {
+public class WebViewCreator extends Application {
 
-    public static void main(String[] args) {
+    public static void init(String[] args) {
         launch(args);
     }
 
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("UNS Flights");
         Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-
+        URL url = this.getClass().getClassLoader().getResource("index.html");
         WebView webView = new WebView();
+
         webView.setPrefHeight(screenBounds.getHeight());
         webView.setFontSmoothingType(FontSmoothingType.GRAY);
 
         WebEngine webEngine = webView.getEngine();
-        webEngine.getLoadWorker().stateProperty().addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                        if (newValue != Worker.State.SUCCEEDED) { return; }
+        webEngine.load(url.toString());
 
-                        JSObject window = (JSObject) webEngine.executeScript("window");
-                        window.setMember("javaSQLBridge", new JavaWebSql());
-                    }
+        // Add the Java-JS communication bridge object into the JS window object
+        webEngine.getLoadWorker().stateProperty().addListener(
+                (ChangeListener<? super Worker.State>) (observable, oldValue, newValue) -> {
+                    if (newValue != Worker.State.SUCCEEDED) { return; }
+
+                    JSObject window = (JSObject) webEngine.executeScript("window");
+                    window.setMember("javaSQLBridge", new JavaWebSql());
                 }
         );
 
-        URL url = this.getClass().getClassLoader().getResource("index.html");
-        webEngine.load(url.toString());
-
-
-
-        VBox vBox = new VBox(webView);
-        Scene scene = new Scene(vBox, screenBounds.getWidth(), screenBounds.getHeight());
-
+        Scene scene = new Scene(new VBox((webView)), screenBounds.getWidth(), screenBounds.getHeight());
+        primaryStage.setTitle("UNS Flights");
         primaryStage.setMaximized(true);
         primaryStage.setScene(scene);
         primaryStage.show();
